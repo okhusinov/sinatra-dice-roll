6cff9820d49119f327c0673afa00cc65da16c60c
okhusinov <okhusinov@chicagobooth.edu>
2025-01-30 07:53:36 +0000

    roll

--- dice.rb
+++ dice.rb
@@ -27,7 +27,7 @@ get("/dice/2/6") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total sum of #{sum}."
 
-  erb(:two_six, { :layout => :wrapper })
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -37,15 +37,15 @@ get("/dice/2/10") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total sum of #{sum}."
 
-  erb(:two_ten, { :layout => :wrapper })
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @die = rand(1..20)
   
-  @outcome = "Your rolled a #{die}."
+  @outcome = "Your rolled a #{@die}."
 
-  erb(:one_twenty, { :layout => :wrapper })
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -58,5 +58,17 @@ get("/dice/5/4") do
 
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and #{fifth_die} for a total sum of #{sum}."
 
-  erb(:five_four, { :layout => :wrapper })
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do 
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
